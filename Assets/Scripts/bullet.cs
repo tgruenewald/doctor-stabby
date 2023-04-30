5c67d9ce672a0d439f9e7a2543946ffb5c9422ee
Python-Dragon <thomas.e.gruenewald@gmail.com>
2023-04-30 12:30:36 -0600

    Shoot (#9)
    * redid flight becasue it broke
    
    * fixed flight
    
    * got bullet and spawer to work

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public Vector2 aim;
+    private GameObject player;
+    public float speed;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+        Debug.Log("hello");
+        aim = (player.transform.position - transform.position).normalized; 
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition (rb.position + aim * Time.deltaTime * speed);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
